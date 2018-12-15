8df5de2d9cbf273f274fdc8f4256943de49c969b
Petar Lemajic <tbug@Petars-MacBook-Pro.local>
2018-12-15 19:09:53 +0100

    Project skeleton

---
+++ NetworkReachability/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkReachability
+//
+//  Created by Petar Lemajic on 12/15/18.
+//  Copyright © 2018 Petar Lemajic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
